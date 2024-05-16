69170002d3445c80452561bd43c2f419b816e317
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-05-16 21:57:13 +0400

    more tests

--- C++ Learning projects/_Learning c++/tempCodeRunnerFile.cpp
+++ C++ Learning projects/_Learning c++/tempCodeRunnerFile.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
 
-     #define __PRETY_FUNCTION__ __FUNCTION__ 
+     #define __PRETTY_FUNCTION__ __FUNCTION__ 
 
 
  void foo(){
-     std::cout << __PRETY_FUNCTION__;
+     std::cout << __PRETTY_FUNCTION__;
  }
 
 #define UNUSED(variable) (void)variable
@@ -12,10 +15,21 @@
 int main() {
 
     int i = 0;
-
-    foo();
-
+    // foo();
     UNUSED(i);
+
+    const auto size = 4ull;
+    int arr[size];
     
+    std::vector<std::string> list_1 {"One", "Two", "Three"};
+    std::vector<std::string> list_2;
+
+    list_2.push_back(list_1.back());
+    list_1.pop_back();
+
+    std::cout << list_2.front() << ' ' << list_1.back();
+
+
+
     return 0;
 }
