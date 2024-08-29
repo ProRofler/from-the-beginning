49fb1251d95c4c49ac63d6c0b8d41ce8e32ee0c9
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-08-29 23:00:37 +0400

    one container solution

--- C++ Learning projects/Exercise/Task_08_anagram.cpp
+++ C++ Learning projects/Exercise/Task_08_anagram.cpp
@@ -7,19 +7,22 @@
 bool is_anagram(const std::string& a, const std::string& b) {
   if (a.length() != b.length()) return false;
 
-  std::vector<int> chars_1(256, 0);
-  std::vector<int> chars_2(256, 0);
+  std::vector<int> chars(256, 0);
 
   for (int i = 0; i < a.length(); i++) {
-    chars_1.at(a.at(i)) += 1;
-    chars_2.at(b.at(i)) += 1;
+    chars.at(a.at(i)) += 1;
+    chars.at(b.at(i)) -= 1;
   }
 
-  return chars_1 == chars_2 ? true : false;
+  for (auto& c : chars) {
+    if (c != 0) return false;
+  }
+
+  return true;
 }
 
 int main() {
-  switch (is_anagram("fakeksadsad", "lokat")) {
+  switch (is_anagram("fak", "rac")) {
     case true:
       std::cout << "It's anagram\n";
       break;
