c81b19b5b693e1bd931875d9ff18642da467f58e
Damien Pierce <157214060+PierceDamien@users.noreply.github.com>
2024-08-29 22:39:13 +0400

    small optimisation

--- C++ Learning projects/Exercise/Task_08_anagram.cpp
+++ C++ Learning projects/Exercise/Task_08_anagram.cpp
@@ -5,14 +5,13 @@
 // strings a b find if they're anagrams
 
 bool is_anagram(const std::string& a, const std::string& b) {
+  if (a.length() != b.length()) return false;
+
   std::vector<int> chars_1(256, 0);
   std::vector<int> chars_2(256, 0);
 
   for (int i = 0; i < a.length(); i++) {
     chars_1.at(a.at(i)) += 1;
-  }
-
-  for (int i = 0; i < b.length(); i++) {
     chars_2.at(b.at(i)) += 1;
   }
 
@@ -20,7 +19,7 @@ bool is_anagram(const std::string& a, const std::string& b) {
 }
 
 int main() {
-  switch (is_anagram("takol", "lokat")) {
+  switch (is_anagram("fakeksadsad", "lokat")) {
     case true:
       std::cout << "It's anagram\n";
       break;
