e8691dc0024fd5d5f3af6b63afa9e07bff78b922
Dmitry Shilin <157214060+ProRofler@users.noreply.github.com>
2025-01-12 11:47:25 +0400

    Update Leetcode_28_find-the-index-of-the-first-occurrence-in-a-string.cpp

--- C++ Learning projects/Leetcode/Leetcode_28_find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ C++ Learning projects/Leetcode/Leetcode_28_find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -20,3 +20,15 @@ class Solution {
   }
 };
 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        for(int i = 0; i < (int)haystack.length() - (int)needle.length() + 1; i++){
+            if(haystack.substr(i, needle.length()) == needle){
+                return i;
+            } 
+        }
+        return -1;
+    }
+};
